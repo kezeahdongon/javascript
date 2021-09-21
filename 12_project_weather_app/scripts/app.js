ad37486f00be06e4e943f54d124c04c0a2e0defb
kezeahdongon <kezeahdongon54@gmail.com>
2021-09-21 18:12:03 +0800

    push

--- 12_project_weather_app/scripts/app.js
+++ 12_project_weather_app/scripts/app.js
@@ -1,4 +1,8 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI =
 
 const updateCity = async (city) => {
 
